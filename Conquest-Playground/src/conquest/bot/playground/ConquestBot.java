0dd777075a4a70dbe1b9bb0c2d8cc38bd9c0a80e
piratte <piratte65@gmail.com>
2017-08-25 14:38:27 +0200

    work in progress

--- Conquest-Playground/src/conquest/bot/playground/ConquestBot.java
+++ Conquest-Playground/src/conquest/bot/playground/ConquestBot.java
@@ -2,6 +2,7 @@ package conquest.bot.playground;
 
 import conquest.bot.BotParser;
 import conquest.bot.custom.AggressiveBot;
+import conquest.bot.custom.SmartBot;
 import conquest.bot.fight.FightSimulation;
 import conquest.bot.fight.FightSimulation.FightAttackersResults;
 import conquest.bot.fight.FightSimulation.FightDefendersResults;
@@ -9,11 +10,8 @@ import conquest.bot.map.RegionBFS;
 import conquest.bot.map.RegionBFS.BFSNode;
 import conquest.bot.map.RegionBFS.BFSVisitResult;
 import conquest.bot.map.RegionBFS.BFSVisitResultType;
-import conquest.bot.state.ChooseCommand;
-import conquest.bot.state.GameBot;
+import conquest.bot.state.*;
 import conquest.bot.state.GameState.RegionState;
-import conquest.bot.state.MoveCommand;
-import conquest.bot.state.PlaceCommand;
 import conquest.engine.Engine.FightMode;
 import conquest.engine.RunGame;
 import conquest.engine.RunGame.Config;
@@ -24,8 +22,9 @@ import conquest.game.world.Region;
 import conquest.view.GUI;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Collections.*;
 
 /**
  * Copy-paste of {@link AggressiveBot}. Feel free to fool around!
@@ -34,8 +33,10 @@ import java.util.List;
 public class ConquestBot extends GameBot
 {
 	
-	FightAttackersResults aRes;
-	FightDefendersResults dRes;
+	private FightAttackersResults aRes;
+	private FightDefendersResults dRes;
+
+	private SmartBot smartBot = new SmartBot();
 	
 	public ConquestBot() {
 		aRes = FightAttackersResults.loadFromFile(new File("Conquest-Playground/FightSimulation-Attackers-A200-D200.obj"));
@@ -92,45 +93,154 @@ public class ConquestBot extends GameBot
 	
 	@Override
 	public List<PlaceCommand> placeArmies(long timeout) {
-	    System.err.println("Place armies timeout: " + timeout);
-		List<PlaceCommand> result = new ArrayList<>();
-		
-		// CLONE REGIONS OWNED BY ME
-		List<RegionState> mine = new ArrayList<>(state.me.regions.values());
-		
-		// SORT THEM ACCORDING TO THEIR SCORE
-		mine.sort((o1, o2) -> {
+        List<List<PlaceCommand>> options = getAllPlacementOpts(state, getInterestingRegions(state, 3));
+        List<PlaceCommand> bestCommands = null;
+        int bestScore = Integer.MIN_VALUE;
+        for (List<PlaceCommand> commands : options) {
+            int score = evaluatePlaceCommands(state, commands);
+            if (score > bestScore) {
+                bestScore = score;
+                bestCommands = commands;
+            }
+        }
+
+		// DISTRIBUTE ARMIES
+        return bestCommands;
+	}
+
+    private void removeUninterestingRegions(List<RegionState> myRegions) {
+        int i = 0;
+        while (i < myRegions.size() && getRegionScore(myRegions.get(i)) > 0) ++i;
+        while (i < myRegions.size()) myRegions.remove(i);
+    }
+
+    private Comparator<RegionState> getRegionNeighbouringComparator() {
+        return (o1, o2) -> {
             int regionScore1 = getRegionScore(o1);
             int regionScore2 = getRegionScore(o2);
             return regionScore2 - regionScore1;
-        });
-		
-		// DO NOT ADD SOLDIER TO REGIONS THAT HAS SCORE 0 (not perspective)
-		int i = 0;
-		while (i < mine.size() && getRegionScore(mine.get(i)) > 0) ++i;
-		while (i < mine.size()) mine.remove(i);
+            };
+    }
 
-		// DISTRIBUTE ARMIES
-		int armiesLeft = state.me.placeArmies;
-		
-		int index = 0;
-		
-		while (armiesLeft > 0) {
-			result.add(new PlaceCommand(mine.get(index).region, 3));
-			armiesLeft -= 3;
-			++index;
-			if (index >= mine.size()) index = 0;
-		}
-		
-		return result;
-	}
-	
-	private int getRegionScore(RegionState o1) {
+    private int computeBestArmiesPlacement(GameState state) {
+        // explore all the various placement options: all to one; all equally; two equally ==> 7 options
+        List<List<PlaceCommand>> options = getAllPlacementOpts(state, getInterestingRegions(state, 3));
+
+        //List<PlaceCommand> bestCommands = null;
+        int bestScore = Integer.MIN_VALUE;
+        for (List<PlaceCommand> commands : options) {
+            int score = evaluatePlaceCommands(state, commands);
+            if (score > bestScore) {
+                bestScore = score;
+                //bestCommands = commands;
+            }
+        }
+        // return the best one
+        return bestScore;
+    }
+
+    private List<RegionState> getInterestingRegions(GameState state, int numOfRegions) {
+        List<RegionState> myRegions = new ArrayList<>(state.me.regions.values());
+        myRegions.sort(getRegionNeighbouringComparator());
+        removeUninterestingRegions(myRegions);
+
+        return getTopRegions(myRegions, numOfRegions);
+    }
+
+    private List<List<PlaceCommand>> getAllPlacementOpts(GameState state, List<RegionState> bestRegions) {
+        List<List<PlaceCommand>> options = new ArrayList<>(7);
+        options.addAll(createAllArmsToOneRegCommands(bestRegions,state));
+        options.add(createUniformAmrsCommands(bestRegions,state));
+        if (bestRegions.size()>2) {
+            options.addAll(createAllArmsToSubsetRegCommands(bestRegions,state));
+        }
+        return options;
+    }
+
+    private List<RegionState> getTopRegions(List<RegionState> myRegions, int NUMBER_OF_REGIONS) {
+        List<RegionState> bestRegions;
+        if (myRegions.size()>NUMBER_OF_REGIONS) {
+            bestRegions = new ArrayList<>(myRegions.subList(0, NUMBER_OF_REGIONS));
+        } else {
+            bestRegions = new ArrayList<>(NUMBER_OF_REGIONS);
+            bestRegions.addAll(myRegions);
+        }
+        return bestRegions;
+    }
+
+    private int evaluatePlaceCommands(GameState state, List<PlaceCommand> placeCommands) {
+        /* for (PlaceCommand cmd : placeCommands) {
+            state.apply(cmd);
+        }*/
+
+        // evaluate
+        int result = computeBestArmiesMovement(state, placeCommands);
+
+        /*for (PlaceCommand cmd : placeCommands) {
+            state.revert(cmd);
+        }*/
+
+        return result;
+    }
+
+    private List<List<PlaceCommand>> createAllArmsToSubsetRegCommands(List<RegionState> bestRegions, GameState state) {
+	    List<List<PlaceCommand>> result = new ArrayList<>(3);
+
+        for (int ignoreIndex = 0; ignoreIndex <bestRegions.size(); ignoreIndex++) {
+            List<PlaceCommand> commands = new ArrayList<>();
+            int armiesLeft = state.me.placeArmies;
+            int index = 0;
+            while (armiesLeft > 0) {
+                if (index != ignoreIndex) {
+                    commands.add(new PlaceCommand(bestRegions.get(index).region, 1));
+                    armiesLeft -= 3;
+                }
+                ++index;
+                if (index >= bestRegions.size()) index = 0;
+            }
+            result.add(commands);
+        }
+
+        return result;
+    }
+
+    private List<PlaceCommand> createUniformAmrsCommands(List<RegionState> bestRegions, GameState state) {
+        List<PlaceCommand> result = new ArrayList<>();
+        int armiesLeft = state.me.placeArmies;
+        int index = 0;
+        while (armiesLeft > 0) {
+            result.add(new PlaceCommand(bestRegions.get(index).region, 1));
+            armiesLeft -= 3;
+            ++index;
+            if (index >= bestRegions.size()) index = 0;
+        }
+        return result;
+    }
+
+    private List<List<PlaceCommand>> createAllArmsToOneRegCommands(List<RegionState> bestRegions, GameState state) {
+        List<List<PlaceCommand>> result = new ArrayList<>(3);
+        for (RegionState bestRegion : bestRegions) {
+            int armiesLeft = state.me.placeArmies;
+            List<PlaceCommand> commands = new ArrayList<>(armiesLeft / 3 + 1);
+            while (armiesLeft > 0) {
+                commands.add(new PlaceCommand(bestRegion.region, 3));
+                armiesLeft -= 3;
+            }
+            result.add(commands);
+        }
+        return result;
+    }
+
+    private int getRegionScore(RegionState o1) {
+	    final int NEUTRAL_WEIGHT = 2;
+	    final int ENEMY_WEIGHT = 5;
+	    final double ARMY_STRENGTH_WEIGHT = 0.5;
 		int result = 0;
 		
 		for (Region reg : o1.region.getNeighbours()) {
-			result += (state.region(reg).owned(Player.NEUTRAL) ? 1 : 0) * 5;
-			result += (state.region(reg).owned(Player.OPPONENT) ? 1 : 0) * 2;
+			result += (state.region(reg).owned(Player.NEUTRAL) ? 1 : 0) * NEUTRAL_WEIGHT;
+			result += (state.region(reg).owned(Player.OPPONENT) ? 1 : 0) * ENEMY_WEIGHT *
+                                            Math.round(1 + ARMY_STRENGTH_WEIGHT * state.region(reg).armies);
 		}
 		
 		return result;
@@ -142,7 +252,30 @@ public class ConquestBot extends GameBot
 
 	@Override
 	public List<MoveCommand> moveArmies(long timeout) {
-        System.err.println("Move armies timeout: " + timeout);
+        List<RegionState> hotRegions = getInterestingRegions(state, 5);
+
+        List<List<MoveCommand>> options = generateMoveOptions(hotRegions);
+
+        // move all latent armies to closest hot region
+        List<RegionState> coldRegions = new ArrayList<>(state.me.regions.values());
+        coldRegions.removeAll(hotRegions);
+        List<MoveCommand> transferCommands = generateTransferCommands(coldRegions, hotRegions);
+        // each option must also bare the transfer commands
+        for (List<MoveCommand> option :options) {
+            option.addAll(transferCommands);
+        }
+
+        List<MoveCommand> bestCommands = null;
+        int bestScore = Integer.MIN_VALUE;
+        for (List<MoveCommand> commands : options) {
+            int score = evaluateMoveCommands(state, EMPTY_LIST, commands);
+            if (score > bestScore) {
+                bestScore = score;
+                bestCommands = commands;
+            }
+        }
+        return bestCommands;
+        /*
 		List<MoveCommand> result = new ArrayList<>();
 		
 		// CAPTURE ALL REGIONS WE CAN
@@ -167,49 +300,162 @@ public class ConquestBot extends GameBot
 		}
 		
 		return result;
-	}
-	
-	private boolean hasOnlyMyNeighbours(RegionState from) {
-		for (RegionState region : from.neighbours) {
-			if (!region.owned(Player.ME)) return false;
-		}
-		return true;
+		*/
 	}
 
-	private int getRequiredSoldiersToConquerRegion(RegionState from, RegionState to, double winProbability) {
-		int attackers = from.armies - 1;
-		int defenders = to.armies;
-		
-		for (int a = defenders; a <= attackers; ++a) {
-			double chance = aRes.getAttackersWinChance(a, defenders);
-			if (chance >= winProbability) {
-				return a;
-			}
-		}
-		
-		return Integer.MAX_VALUE;
-	}
-		
-	private boolean shouldAttack(RegionState from, RegionState to, double winProbability) {	
-		return from.armies > getRequiredSoldiersToConquerRegion(from, to, winProbability);
-	}
-	
-	private MoveCommand attack(RegionState from, RegionState to, double winProbability) {
-        return new MoveCommand(from.region, to.region, getRequiredSoldiersToConquerRegion(from, to, winProbability));
-	}
-	
+    private int computeBestArmiesMovement(GameState state, List<PlaceCommand> placeCommands) {
+        List<RegionState> hotRegions = getInterestingRegions(state, 5);
+
+        List<List<MoveCommand>> options = generateMoveOptions(hotRegions);
+
+        // move all latent armies to closest hot region
+        List<RegionState> coldRegions = new ArrayList<>(state.me.regions.values());
+        coldRegions.removeAll(hotRegions);
+        List<MoveCommand> transferCommands = generateTransferCommands(coldRegions, hotRegions);
+        // each option must also bare the transfer commands
+        for (List<MoveCommand> option :options) {
+            option.addAll(transferCommands);
+        }
+
+        // return the best score achievable
+        //List<MoveCommand> bestCommands = null;
+        int bestScore = Integer.MIN_VALUE;
+        for (List<MoveCommand> commands : options) {
+            int score = evaluateMoveCommands(state, placeCommands,commands);
+            if (score > bestScore) {
+                bestScore = score;
+                //bestCommands = commands;
+            }
+        }
+
+        return bestScore;
+    }
+
+    private List<List<MoveCommand>> generateMoveOptions(List<RegionState> hotRegions) {
+        List<List<List<MoveCommand>>> allRegionOptions = new ArrayList<>(hotRegions.size());
+        // evaluate them
+        for (RegionState myRegion : hotRegions) {
+            List<RegionState> neighbours = new ArrayList<>(myRegion.neighbours.length);
+            List<List<MoveCommand>> regionOptions = new ArrayList<>(myRegion.neighbours.length*2);
+            final int availableArmies = myRegion.armies - 1;
+            for (RegionState to : myRegion.neighbours) {
+                // DO NOT ATTACK OWN REGIONS
+                if (to.owned(Player.ME)) continue;
+                neighbours.add(to);
+            }
+            // make commands for regions and doubles of regs
+            // singles
+            for (RegionState neighbour : neighbours) {
+                regionOptions.add(singletonList(new MoveCommand(myRegion.region, neighbour.region, availableArmies)));
+            }
+
+            // doubles
+            for (int regIndex = 0; regIndex < neighbours.size()-1; regIndex++) {
+                for (int restIndex = regIndex+1; restIndex < neighbours.size(); restIndex++) {
+                    regionOptions.add(Arrays.asList(
+                            new MoveCommand(myRegion.region, neighbours.get(regIndex).region, Math.round(availableArmies/2)),
+                            new MoveCommand(myRegion.region, neighbours.get(restIndex).region, Math.round(availableArmies/2))
+                    ));
+                }
+            }
+            allRegionOptions.add(regionOptions);
+        }
+
+        // make all combinations of commands for all regs
+        List<List<MoveCommand>> options = new ArrayList<>();
+        generatePermutations(allRegionOptions, options, 0, new ArrayList<>());
+        return options;
+    }
+
+    private List<MoveCommand> generateTransferCommands(List<RegionState> coldRegions, List<RegionState> hotRegions) {
+        List<MoveCommand> result = new ArrayList<>();
+	    for (RegionState coldRegion : coldRegions) {
+	        // find closest hot regions
+            MoveCommand newCommand = moveToFront(coldRegion, hotRegions);
+            if (newCommand == null) {
+                System.err.println("MoveToFront method failed to find way to front!");
+            } else {
+                result.add(newCommand);
+            }
+        }
+        return result;
+    }
+
+    private void generatePermutations(List<List<List<MoveCommand>>> Lists, List<List<MoveCommand>> result, int depth, List<MoveCommand>current)
+    {
+        if(depth == Lists.size()) {
+            result.add(current);
+            return;
+        }
+
+        for(int i = 0; i < Lists.get(depth).size(); ++i) {
+            current.addAll(Lists.get(depth).get(i));
+            generatePermutations(Lists, result, depth + 1, current);
+        }
+    }
+
+    private int evaluateMoveCommands(GameState state, List<PlaceCommand> placeCommands, List<MoveCommand> moveCommands) {
+        /*for (MoveCommand cmd : moveCommands) {
+            state.apply(cmd);
+        }*/
+
+        // evaluate
+        int result = computeOpponentMove(state, placeCommands, moveCommands);
+
+        /*for (MoveCommand cmd : moveCommands) {
+            state.revert(cmd);
+        }*/
+
+        return result;
+    }
+
+    private int computeOpponentMove(GameState state, List<PlaceCommand> placeCommands, List<MoveCommand> moveCommands) {
+        // simulate opponent move
+        final int IGNORED_TIMEOUT = 0;
+        smartBot.enforceState(state);
+        List<PlaceCommand> oponentPlace = smartBot.placeArmies(IGNORED_TIMEOUT);
+        List<MoveCommand> oponentMove = smartBot.moveArmies(IGNORED_TIMEOUT);
+
+        // simulate simulator computation
+        int result = runSimulator(state, placeCommands, moveCommands, oponentPlace, oponentMove);
+
+        return result;
+    }
+
+    private int runSimulator(GameState state,
+                             List<PlaceCommand> placeCommands, List<MoveCommand> moveCommands,
+                             List<PlaceCommand> oponentPlace, List<MoveCommand> oponentMove) {
+	    // TODO
+
+	    // check for clashing commands
+
+        // apply non-clashing commands
+
+        // branch on clashing commands
+
+        // branch on battles that could have two outcomes
+
+        // weight results for one outcome
+
+        // ADD END OF RECURSION
+
+        // revert applied commands
+
+        return 0;
+    }
+
 	private MoveCommand transfer(RegionState from, RegionState to) {
         return new MoveCommand(from.region, to.region, from.armies-1);
 	}
 	
 	private Region moveToFrontRegion;
 	
-	private MoveCommand moveToFront(RegionState from) {
+	private MoveCommand moveToFront(RegionState from, List<RegionState> front) {
 		RegionBFS<BFSNode> bfs = new RegionBFS<>();
 		moveToFrontRegion = null;
-		bfs.run(from.region, (region, level, parent, thisNode) -> {
+		bfs.run(from.region, (Region region, int level, BFSNode parent, BFSNode thisNode) -> {
             //System.err.println((parent == null ? "START" : parent.level + ":" + parent.region) + " --> " + level + ":" + region);
-            if (!hasOnlyMyNeighbours(state.region(region))) {
+            if (front.contains(state.region(region))) {
                 moveToFrontRegion = region;
                 return new BFSVisitResult<>(BFSVisitResultType.TERMINATE, thisNode == null ? new BFSNode() : thisNode);
             }
@@ -217,10 +463,9 @@ public class ConquestBot extends GameBot
         });
 		
 		if (moveToFrontRegion != null) {
-			//List<Region> path = fw.getPath(from.getRegion(), moveToFrontRegion);
 			List<Region> path = bfs.getAllPaths(moveToFrontRegion).get(0);
 			Region moveTo = path.get(1);
-			
+
 			boolean first = true;
 			for (Region region : path) {
 				if (first) first = false;
@@ -228,10 +473,8 @@ public class ConquestBot extends GameBot
 				System.err.print(region);
 			}
 			System.err.println();
-			
 			return transfer(from, state.region(moveTo));
 		}
-		
 		return null;
 	}
 	
@@ -243,6 +486,7 @@ public class ConquestBot extends GameBot
 		config.bot1Init = "internal:conquest.bot.playground.ConquestBot";
 		//config.bot1Init = "dir;process:../Conquest-Bots;java -cp ./bin;../Conquest/bin conquest.bot.external.JavaBot conquest.bot.playground.ConquestBot ./ConquestBot.log";
 		config.bot2Init = "internal:conquest.bot.BotStarter";
+        //config.bot2Init = "internal:conquest.bot.custom.SmartBot";
 		//config.bot2Init = "human";
 		
 		config.engine.botCommandTimeoutMillis = 24*60*60*1000;
